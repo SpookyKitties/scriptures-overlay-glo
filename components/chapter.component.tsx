9b48f7cd80461efea808b17ef040ceb837664bd8
Jared Parkinson <jared@parkinson.im>
2019-11-23 02:32:09 -0700

    layout

--- components/chapter.component.tsx
+++ components/chapter.component.tsx
@@ -279,7 +279,17 @@ export class ChapterComponent extends Component<ChapterProps> {
             : "chapter-content manual"
         }
       >
-        <span></span>
+        <span className={"left-nav"}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M11.67 3.87L9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z" />
+            <path fill="none" d="M0 0h24v24H0z" />
+          </svg>
+        </span>
         <div
           id={this.props.chapter.id}
           className="chapter"
@@ -291,7 +301,17 @@ export class ChapterComponent extends Component<ChapterProps> {
         </header> */}
           {renderFormatGroups(this.props.chapter.body.grps)}
         </div>
-        <span></span>
+        <span className={"right-nav"}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5.88 4.12L13.76 12l-7.88 7.88L8 22l10-10L8 2z" />
+            <path fill="none" d="M0 0h24v24H0z" />
+          </svg>
+        </span>
       </div>
     );
   }
