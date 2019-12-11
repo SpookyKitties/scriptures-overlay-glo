5c52341c34ddc4ded13311c903527f0956dac30f
Jared Parkinson <jared@parkinson.im>
2019-12-11 02:35:19 -0700

    adding offseting in the correct spot

--- components/verse-notes/notePhraseClick.ts
+++ components/verse-notes/notePhraseClick.ts
@@ -1,9 +1,13 @@
 import { FormatTagNoteOffsets } from '../../oith-lib/src/verse-notes/verse-note';
 import { of } from 'rxjs';
-import { map, delay, find, flatMap } from 'rxjs/operators';
+import { map, delay, find, flatMap, filter } from 'rxjs/operators';
 import { store, formatTagService } from '../header.component';
+import {
+  expandOffsets,
+  compressRanges,
+} from '../../oith-lib/src/offsets/expandOffsets';
 
-function checkSelection(e: Element) {
+function checkSelection(e: Element, formatTag: FormatTagNoteOffsets) {
   const selection = document.getSelection();
 
   console.log(selection);
@@ -30,21 +34,31 @@ function checkSelection(e: Element) {
         const noteID = note.id;
         'eng-heb-1-1-verse-notes';
         // console.log(note.id);
+        const noteIDSplit = noteID.split('-');
 
-        if (verseID) {
-          const noteIDSplit = noteID.split('-');
+        if (verseID && verseID[2] === noteIDSplit[noteIDSplit.length - 3]) {
+          console.log(verseID);
 
-          console.log(verseID[2] === noteIDSplit[noteIDSplit.length - 3]);
+          formatTag.offsets = `${formatTag.offsets},${start}-${end}`;
+          // console.log()
 
-          console.log(start);
-          console.log(end);
-          console.log(verse.textContent.slice(start, end));
-          console.log(verse);
-          console.log(note);
+          return expandOffsets(formatTag).pipe(
+            map(
+              () =>
+                (formatTag.offsets = compressRanges(
+                  formatTag.uncompressedOffsets,
+                )
+                  .map(i => i.join('-'))
+                  .join(',')),
+            ),
+            map(() => false),
+          );
         }
       }
     }
   }
+
+  console.log('oiajsdfioajsdfiojasdf');
 
   return of(true);
 }
@@ -53,17 +67,23 @@ export function notePhraseClick(
   evt: HTMLElement,
   formatTag: FormatTagNoteOffsets,
 ) {
-  checkSelection(evt);
-  formatTagService
-    .notePhaseClick(formatTag)
+  checkSelection(evt, formatTag)
     .pipe(
+      filter(o => o === true),
       map(() => {
-        store.updateFTags$.next(true);
-        store.updateNoteVisibility$.next(true);
+        return formatTagService.notePhaseClick(formatTag).pipe(
+          map(() => {
+            store.updateFTags$.next(true);
+            store.updateNoteVisibility$.next(true);
+          }),
+          delay(100),
+        );
       }),
-      delay(100),
+      flatMap(o => o),
     )
     .subscribe(() => {
+      console.log('asdiojasofjaoisfj');
+
       of(document.querySelectorAll('.verse'))
         .pipe(
           flatMap(o => o),
@@ -75,9 +95,5 @@ export function notePhraseClick(
           }),
         )
         .subscribe();
-      // const vng = document.querySelector('.f-t.highlight'); //.scrollIntoView();
-      // if (vng) {
-      //   vng.scrollIntoView();
-      // }
     });
 }
