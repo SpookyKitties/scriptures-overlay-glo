42191c34f75de9108849bafe1846c3adddc76210
Jared Parkinson <jared@parkinson.im>
2020-01-12 13:40:24 -0700

    analytic tests

--- components/gotoLink.tsx
+++ components/gotoLink.tsx
@@ -1,6 +1,7 @@
-import { MouseEvent } from "react";
-import Router from "next/router";
-import { store } from "./SettingsComponent";
+import { MouseEvent } from 'react';
+import Router from 'next/router';
+import { store } from './SettingsComponent';
+import ReactGa from 'react-ga';
 
 function gotoExternalLink(href: string) {}
 
@@ -8,21 +9,27 @@ export function gotoLink(event: MouseEvent, href?: string) {
   event.preventDefault();
   const h = href ? href : (event.target as HTMLAnchorElement).href;
   if (href) {
-    Router.push("/[book]/[chapter]", href);
+    Router.push('/[book]/[chapter]', href);
     store.history = false;
     return;
   }
   if (h) {
-    if (h.includes("churchofjesuschrist")) {
+    if (h.includes('churchofjesuschrist')) {
       return gotoExternalLink(h);
     }
     if (href) {
-      Router.push("/[book]/[chapter]", href);
+      Router.push('/[book]/[chapter]', href);
       store.history = false;
       return;
     }
     const url = /(^.+)(\/.+\/.+)/g.exec(h);
-    Router.push("/[book]/[chapter]", url[2]);
+
+    ReactGa.event({
+      category: 'cross-ref',
+      action: `${window.location.href}`,
+      label: url[2],
+    });
+    Router.push('/[book]/[chapter]', url[2]);
     store.history = false;
   }
 }
