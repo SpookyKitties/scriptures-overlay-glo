eb7c9b1097204f9adf5aae1a1e1e8f07b82d0693
Jared Parkinson <jared@parkinson.im>
2020-01-12 13:54:41 -0700

    analytics update

--- components/gotoLink.tsx
+++ components/gotoLink.tsx
@@ -26,7 +26,7 @@ export function gotoLink(event: MouseEvent, href?: string) {
 
     ReactGa.event({
       category: 'cross-ref',
-      action: `${window.location.pathname}`,
+      action: `${window.location.pathname.split('.')[0]}`,
       label: url[2],
     });
     Router.push('/[book]/[chapter]', url[2]);
