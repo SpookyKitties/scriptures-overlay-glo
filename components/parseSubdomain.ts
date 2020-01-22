4aa17916c4f08d7b7be9051d8df4258e613d0ceb
Jared Parkinson <jared@parkinson.im>
2020-01-22 03:53:36 -0700

    bugs

--- components/parseSubdomain.ts
+++ components/parseSubdomain.ts
@@ -12,7 +12,7 @@ export function parseSubdomain(host?: string) {
         return '';
       }
       if (subDomain.toLowerCase() === 'b') {
-        return 'future';
+        return 'junetest';
       }
 
       return subDomain;
