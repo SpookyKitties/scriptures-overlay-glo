24da3c6e2453faca554ec79f0161adf9e02a8f51
Jared Parkinson <jared@parkinson.im>
2020-02-04 05:06:43 -0700

    bugs

--- components/scroll.tsx
+++ components/scroll.tsx
@@ -1,7 +1,7 @@
-import { filter, map, take, debounceTime } from 'rxjs/operators';
+import { filter, map, take, debounceTime, flatMap } from 'rxjs/operators';
 import { store } from './SettingsComponent';
 import { syncedVerse } from './mobile-notes.tsx/MobileNotesComponent';
-import { Subject } from 'rxjs';
+import { Subject, EMPTY } from 'rxjs';
 
 function resetVerseFocus() {
   Array.from(document.querySelectorAll('.verse.focused')).map(v =>
@@ -26,45 +26,50 @@ export function scrollTop(selector: string, count: number) {
 }
 const syncScrolling = new Subject<void>();
 
-syncScrolling.pipe(debounceTime(150)).subscribe(() => {
-  const verses = Array.from(document.querySelectorAll('.verse'));
-  const chapterElement = document.querySelector('.chapter-loader');
-  if (chapterElement) {
-    const y = chapterElement.getBoundingClientRect().top;
-    const verse = verses.find(
-      e => e.getBoundingClientRect().bottom - 10 >= y === true,
-    );
+syncScrolling
+  .pipe(
+    map(() => {
+      const verses = Array.from(document.querySelectorAll('.verse'));
+      const chapterElement = document.querySelector('.chapter-loader');
+      if (chapterElement) {
+        const y = chapterElement.getBoundingClientRect().top;
+        const verse = verses.find(
+          e => e.getBoundingClientRect().bottom - 10 >= y === true,
+        ) as Element;
 
-    if (verse) {
-      store.chapter
-        .pipe(
-          take(1),
-          filter(o => o !== undefined),
-          map(chapter => {
-            resetVerseFocus();
-            const tempID = /^(p)(.+)$/g.exec(verse.id);
-            const id = tempID ? tempID[2] : verse.id;
+        if (verse) {
+          return store.chapter.pipe(
+            take(1),
+            filter(o => o !== undefined),
+            map(chapter => {
+              resetVerseFocus();
+              const tempID = /^(p)(.+)$/g.exec(verse.id);
+              const id = tempID ? tempID[2] : verse.id;
 
-            const vn = chapter.verseNotes.find(vn =>
-              vn.id.includes(`-${id}-verse-note`),
-            );
-            if (syncedVerse) {
-              setVerseFocus(verse.id);
-              syncedVerse.next(vn);
-            }
-            const verseNote = document.querySelector(
-              `[id*='-${id}-verse-note']`,
-            );
-            if (verseNote) {
-              verseNote.scrollIntoView();
-              // scrollTop('.verse-notes', -48);
-            }
-          }),
-        )
-        .subscribe();
+              const vn = chapter.verseNotes.find(vn =>
+                vn.id.includes(`-${id}-verse-note`),
+              );
+              if (syncedVerse) {
+                setVerseFocus(verse.id);
+                syncedVerse.next(vn);
+              }
+              return id;
+            }),
+          );
+          // .subscribe();
+        }
+      }
+      return EMPTY;
+    }),
+    flatMap(o => o),
+  )
+  .subscribe(id => {
+    const verseNote = document.querySelector(`[id*='-${id}-verse-note']`);
+    if (verseNote) {
+      verseNote.scrollIntoView();
+      // scrollTop('.verse-notes', -48);
     }
-  }
-});
+  });
 
 export function scroll() {
   syncScrolling.next();
